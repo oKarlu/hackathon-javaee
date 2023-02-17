2ce1ff850bcc84ea25ccf7675fd38ae193c87e62
Carlos Marinho <cadumsantos@hotmail.com>
2023-02-17 19:34:37 -0300

    + Criação de dados para carga inicial do projeto

---
+++ src/main/resources/db/carga-inicial.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios VALUES ('Maria', 'maria', 'maria@email.com', '123456', '1980-03-14', '2019-04-26 11:22:22', '2022-08-05 06:23:45');
+INSERT INTO usuarios VALUES ('Joao', 'joao','joao@email.com','654321', '1998-08-27', '2022-05-10 10:45:27', '2022-11-23 09:10:33');
+INSERT INTO usuarios VALUES ('Paulo Henrique', 'ph', 'ph@email.com', '11111', '1997-10-26', '2020-06-13 14:50:06', '2021-02-16 12:17:55');
+INSERT INTO usuarios VALUES ('Jhonatan', 'Jhonatan', 'jhonatan@email.com', '555555', '1999-06-05', '2021-07-06 22:08:11', '2021-12-03 05:55:04');
+INSERT INTO usuarios VALUES ('Geovana', 'geovana', 'geovana@email.com', '787878', '2000-02-13', '2023-08-15 10:45:15', '2023-10-19 15:28:37');
