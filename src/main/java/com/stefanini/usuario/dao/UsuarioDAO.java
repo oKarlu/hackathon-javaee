4560295f045457a4ea95902d65d9f36f6ee6100b
Carlos Marinho <cadumsantos@hotmail.com>
2023-02-17 17:56:22 -0300

    + Criação da UsuarioDAO
    Criação da Classe UsuarioDAO estendendo da classe GenericDAO<> para acesso a dados

---
+++ src/main/java/com/stefanini/usuario/dao/UsuarioDAO.java
@@ -0,0 +1,11 @@
+package com.stefanini.usuario.dao;
+
+import com.stefanini.dao.GenericDAO;
+import com.stefanini.usuario.model.UsuarioModel;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UsuarioDAO extends GenericDAO<UsuarioModel, Long> {
+    
+}
